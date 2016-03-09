16a91d88a78e0e3b4a3d10921f704e4d03cc8701
UnJavaScripter <eltu.khan@gmail.com>
2016-03-09 10:27:47 -0500

    add the local json reference again

--- src/app/services/data-service/data-service.ts
+++ src/app/services/data-service/data-service.ts
@@ -18,7 +18,8 @@ export class DataService {
 
     getData() {
         let that = this;
-        const URL = 'http://eventhosts-unjavascripter.rhcloud.com/api/hosts';
+        //const URL = 'http://eventhosts-unjavascripter.rhcloud.com/api/hosts';
+        const URL = '../../../hosts_data.json';
 
         // return an observable
         return this.http.get(URL)
